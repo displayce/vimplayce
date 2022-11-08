4d87be70e1110e06d72f584b8e92685b5440afe3
Thomas TEIXEIRA <avanalos@gmail.com>
2022-11-08 23:47:35 +0100

    feat: reload_config not global now but a one-time autocmd

--- nvim/lua/vimplayce/core/packer.lua
+++ nvim/lua/vimplayce/core/packer.lua
@@ -5,14 +5,21 @@ local function ensure_packer()
 	if fn.empty(fn.glob(install_path)) > 0 then
 		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 
-		-- automatically reload vimplayce configuration when boostrapping
-		-- packer and jump to its homepage on sync end.
+		-- automatically reload vimplayce configuration when 
+		-- syncing packer and jump to homepage.
 		vim.api.nvim_create_autocmd('User', {
 			pattern = 'PackerComplete',
+			-- MAYBE: remove the once so when packer is synced, the whole config reload
 			once = true,
 			callback = function()
 				vim.api.nvim_win_close(0, true)
-				require('vimplayce.core.utils').reload_config()
+				for name in pairs(package.loaded) do
+					if name:match('^vimplayce') then
+						package.loaded[name] = nil
+					end
+				end
+
+				dofile(vim.env.MYVIMRC)
 				require('mini.starter').open(1)
 			end
 		})
