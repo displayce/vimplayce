d9398c74662b6981bf3888cf930d070bc6cc292e
Thomas Teixeira <thomas.teixeira@displayce.com>
2023-01-05 18:17:36 +0100

    feat: change bindings

--- nvim/lua/vimplayce/core/config/keymaps.lua
+++ nvim/lua/vimplayce/core/config/keymaps.lua
@@ -20,24 +20,21 @@ map("n", "<leader>bb", ":buffers<cr>:buffer ", { noremap = true })
 
 -- Window mappings
 -- splits
-map("n", "<leader>wv", "<C-W>v", opts)
-map("n", "<leader>ws", "<C-W>s", opts)
-map("n", "<leader>wf", ":vnew ", { noremap = true })
+map("n", "<leader>wsv", "<C-W>v", opts)
+map("n", "<leader>wsh", "<C-W>s", opts)
+map("n", "<leader>wsn", ":vnew ", { noremap = true })
+map("n", "<leader>wsc", "<C-W>c", opts)
+map("n", "<leader>wso", "<C-W>o", opts)
 -- select
 map("n", "<leader>wh", "<C-W>h", opts)
 map("n", "<leader>wj", "<C-W>j", opts)
 map("n", "<leader>wk", "<C-W>k", opts)
 map("n", "<leader>wl", "<C-W>l", opts)
 -- move
-map("n", "<leader>wH", "<C-W>H", opts)
-map("n", "<leader>wJ", "<C-W>J", opts)
-map("n", "<leader>wK", "<C-W>K", opts)
-map("n", "<leader>wL", "<C-W>L", opts)
-map("n", "<leader>wr", "<C-W>r", opts)
-map("n", "<leader>wR", "<C-W>R", opts)
+map("n", "<leader>wmh", "<C-W>H", opts)
+map("n", "<leader>wmj", "<C-W>J", opts)
+map("n", "<leader>wmk", "<C-W>K", opts)
+map("n", "<leader>wml", "<C-W>L", opts)
 -- resize
-map("n", "<leader>wF", ":resize <bar> :vertical resize<CR>", opts)
-map("n", "<leader>wE", "<C-W>=", opts)
--- close
-map("n", "<leader>wc", "<C-W>c", opts)
-map("n", "<leader>wo", "<C-W>o", opts)
+map("n", "<leader>wrf", ":resize <bar> :vertical resize<CR>", opts)
+map("n", "<leader>wre", "<C-W>=", opts)
