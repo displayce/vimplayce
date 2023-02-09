ae13d9dbd1458ff63105885795de96262bb620b5
Thomas Teixeira <thomas.teixeira@displayce.com>
2023-02-09 10:57:21 +0100

    ref: add function so I don't repeating <leader>

--- nvim/lua/vimplayce/core/config/keymaps.lua
+++ nvim/lua/vimplayce/core/config/keymaps.lua
@@ -1,5 +1,9 @@
-local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
+
+local map = vim.keymap.set
+local leadermap = function(mode, mapping, cmd, opts)
+	vim.keymap.set(mode, "<leader>" .. mapping, cmd, opts)
+end
 
 -- Disable space for leader usage
 map("", "<Space>", "<Nop>", opts)
@@ -7,35 +11,35 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- NOH
-map("n", "<leader>h", ":noh<CR>", opts)
+leadermap("n", "h", ":noh<CR>", opts)
 
 -- Yank & Paste
-map({ "n", "v" }, "<leader>y", '"+y', opts)
-map({ "n", "v" }, "<leader>p", '"+p', opts)
+leadermap({ "n", "v" }, "y", '"+y', opts)
+leadermap({ "n", "v" }, "p", '"+p', opts)
 
 -- Buffers mappings
-map("n", "<leader>bh", ":bp<cr>", opts)
-map("n", "<leader>bl", ":bn<cr>", opts)
-map("n", "<leader>bb", ":buffers<cr>:buffer ", { noremap = true })
+leadermap("n", "bh", ":bp<cr>", opts)
+leadermap("n", "bl", ":bn<cr>", opts)
+leadermap("n", "bb", ":buffers<cr>:buffer ", { noremap = true })
 
 -- Window mappings
 -- closing
-map("n", "<leader>wc", "<C-W>c", opts)
-map("n", "<leader>wo", "<C-W>o", opts)
+leadermap("n", "wc", "<C-W>c", opts)
+leadermap("n", "wo", "<C-W>o", opts)
 -- splits
-map("n", "<leader>wv", "<C-W>v", opts)
-map("n", "<leader>ws", "<C-W>s", opts)
-map("n", "<leader>wn", ":vnew ", { noremap = true })
+leadermap("n", "wv", "<C-W>v", opts)
+leadermap("n", "ws", "<C-W>s", opts)
+leadermap("n", "wn", ":vnew ", { noremap = true })
 -- select
-map("n", "<leader>wh", "<C-W>h", opts)
-map("n", "<leader>wj", "<C-W>j", opts)
-map("n", "<leader>wk", "<C-W>k", opts)
-map("n", "<leader>wl", "<C-W>l", opts)
+leadermap("n", "wh", "<C-W>h", opts)
+leadermap("n", "wj", "<C-W>j", opts)
+leadermap("n", "wk", "<C-W>k", opts)
+leadermap("n", "wl", "<C-W>l", opts)
 -- resize
-map("n", "<leader>wf", ":resize <bar> :vertical resize<CR>", opts)
-map("n", "<leader>we", "<C-W>=", opts)
+leadermap("n", "wf", ":resize <bar> :vertical resize<CR>", opts)
+leadermap("n", "we", "<C-W>=", opts)
 -- move
-map("n", "<leader>wmh", "<C-W>H", opts)
-map("n", "<leader>wmj", "<C-W>J", opts)
-map("n", "<leader>wmk", "<C-W>K", opts)
-map("n", "<leader>wml", "<C-W>L", opts)
+leadermap("n", "wmh", "<C-W>H", opts)
+leadermap("n", "wmj", "<C-W>J", opts)
+leadermap("n", "wmk", "<C-W>K", opts)
+leadermap("n", "wml", "<C-W>L", opts)
