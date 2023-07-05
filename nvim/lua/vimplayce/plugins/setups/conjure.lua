2153b272f10e7cbbc25b7055fbd9ffcae927d88b
Thomas Teixeira <thomas.teixeira@displayce.com>
2023-07-05 13:12:47 +0200

    feat: no init file when launching csi for chicken

--- nvim/lua/vimplayce/plugins/setups/conjure.lua
+++ nvim/lua/vimplayce/plugins/setups/conjure.lua
@@ -3,6 +3,6 @@ return function()
 	vim.g["conjure#filetype#python"] = vim.v["false"]
 	vim.g["conjure#filetype#lua"] = vim.v["false"]
 	-- Using CHICKEN scheme as my implementation
-	vim.g["conjure#client#scheme#stdio#command"] = "csi -quiet -:c"
+	vim.g["conjure#client#scheme#stdio#command"] = "csi -quiet -no-init -:c"
 	vim.g["conjure#client#scheme#stdio#prompt_pattern"] = "\n-#;%d-> "
 end
