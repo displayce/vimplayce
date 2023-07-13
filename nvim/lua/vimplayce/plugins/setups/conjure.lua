8ea9d250f92bc9131ee3f4febee4142ddf9351cc
Thomas Teixeira <thomas.teixeira@startmail.com>
2023-07-13 10:49:19 +0200

    feat: disable SQL builtin conjure
    Should look into that at some point though

--- nvim/lua/vimplayce/plugins/setups/conjure.lua
+++ nvim/lua/vimplayce/plugins/setups/conjure.lua
@@ -2,6 +2,7 @@ return function()
 	vim.g["conjure#filetype#rust"] = vim.v["false"]
 	vim.g["conjure#filetype#python"] = vim.v["false"]
 	vim.g["conjure#filetype#lua"] = vim.v["false"]
+	vim.g["conjure#filetype#sql"] = vim.v["false"]
 	-- Using CHICKEN scheme as my implementation
 	vim.g["conjure#client#scheme#stdio#command"] = "csi -quiet -no-init -:c"
 	vim.g["conjure#client#scheme#stdio#prompt_pattern"] = "\n-#;%d-> "
